d98f8ff2cf120394d779542380f07843ea2306a1
rbecker8 <rbecker8@cnm.edu>
2018-07-18 14:39:30 -0600

    Changed location of colons.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -8,28 +8,28 @@
 		<h1>PWP Milestone 1</h1>
 	</header>
 	<body>
-		<h2>Purpose, Audience & Goal</h2>
+		<h2>Purpose, Audience & Goal:</h2>
 		<p><strong>Purpose</strong>
-			: This project will be a professional portfolio website with a focus on front-end web development concepts such as UX/UI.
+			This project will be a professional portfolio website with a focus on front-end web development concepts such as UX/UI.
 		</p>
 		<p>
-			<strong>Audience</strong>
-			: This site is designed with a professional audience in mind.  Targeting individuals in the web development and I.T. community nationwide.
+			<strong>Audience:</strong>
+			This site is designed with a professional audience in mind.  Targeting individuals in the web development and I.T. community nationwide.
 			Target audience to include: startup founders, hiring managers, recruiters, and peers.
 		</p>
 		<p>
-			<strong>Goal</strong>
-			: The primary goal of this site is to provide users with a direct way to contact and communicate with me in regards to employment and collaboration opportunities
+			<strong>Goal:</strong>
+			The primary goal of this site is to provide users with a direct way to contact and communicate with me in regards to employment and collaboration opportunities
 			through the contact form, and/or connected social media profiles.  The secondary goal of this site is to showcase my work, and to contribute to a polished professional online presence.
 		</p>
 		<h2>Persona</h2>
 		<p>
-			<strong>Name</strong>
-			: Bill Davis
+			<strong>Name:</strong>
+			Bill Davis
 		</p>
 		<p>
-			<strong>Age</strong>
-			:38
+			<strong>Age:</strong>
+			38
 		</p>
 		<p>
 			<strong>Profession</strong>
