d0cc0530b44865f2139caff72f6a5abf97958627
rbecker8 <rbecker8@cnm.edu>
2018-07-18 15:24:52 -0600

    Added frustrations and needs section with content to persona.  Added goals section with content to persona.  Added user story section with content to persona.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -40,8 +40,27 @@
 		<p><strong>Technology:</strong>
 			Apple Guru.  Works on an iMac Pro connected to a 27" LG UltraFine 5k display while in the office, a MacBook Pro while at home and keeps up to date on the go using an iPhone X.
 		</p>
-		<p>Attitudes and behaviors:
-		Spends a lot of time browsing the web and keeping up to date on
+		<p><strong>Attitudes and behaviors:</strong>
+			Spends a lot of time browsing the web and keeping up to date on current events.  Especially as they relate to the tech industry.  Heavily involved with social media and believes someone's
+			online presences is the most important thing when looking to hire someone.  A good resume exhibits good organizational skills but he rarely spends much time looking over them.  He feels
+			interacting with potential hires is the best way to get a feel for them.  He wants to make sure personality matches online presence and style.
+		</p>
+		<p><strong>Frustrations and Needs:</strong>
+			Bill is looking for someone who can grow with the company.  He is not looking for a senior level developer, but someone who has 1-2 years of real working experience as well as some
+			formal education related to the field such as BS in computer sciences, physics, math, engineering or similar field.  Someone who can be mentored and trained to work within the company's
+			culture of high tech hardware and software.  He has been getting some resumes, but no one has really stood out of the crowd.  He frequently visits local networking and community events
+			in hopes of finding someone who can present well in a short informal setting.  He is also open to any recommendations from his colleagues and friends with in the community.
+		</p>
+		<p><strong>Goals:</strong>
+			Bill is all about online presence!  Does this candidate have a professional LinkedIn profile? How about twitter?  How about a GitHub profile with detailed repositories?
+			In our ever increasing digital world, he sees a resume as secondary to how well your site presents itself.  Well written content is all good but if it's not presented in a clean concise
+			fashion than it might as well be blank.  Bill needs to know pretty swiftly if this potential person is worth investing further research as well as setting up some sort of interaction.  Whether
+			that be a phone conversation or a full blown interview.
+		</p>
+		<p><strong>User Story:</strong>
+			As a user, I need to quickly assess whether this person presents content in a concise and appealing way as well as how involved they are in our local tech community.
+
+
 		</p>
 	</body>
 </html>
