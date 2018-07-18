351ea2b1db9e00d8d7f770bb2b0fffce1dd613a0
Ryan Becker <rbecker8@cnm.edugit config --global user.name Ryan>
2018-07-18 10:29:16 -0600

    Setup directory structure for project and added basic HTML page structure.

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>PWP Milestone 1</title>
+	</head>
+	<h1 class="main-title">PWP Milestone 1</h1>
+	<body>
+
+
+
+	</body>
+</html>
