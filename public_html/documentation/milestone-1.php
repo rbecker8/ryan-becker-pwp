725ea66d19ab31e525f812455c851192d2486b8c
rbecker8 <rbecker8@cnm.edu>
2018-07-18 11:41:46 -0600

    Added strong to audience and purpose.  Adding formatting to all three sections: purpose, audience and goal.  Added goal section with primary/secondary goals.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -8,9 +8,18 @@
 		<h1>PWP Milestone 1</h1>
 	</header>
 	<body>
-		<p>Purpose: This project will be a professional portfolio website with a focus on front-end web development concepts such as UX/UI</p>
-		<p>Audience: This site is designed with a professional audience in mind.  Targeting individuals in the web development and I.T. community nationwide.<br>
-		Target audience to include: startup founders, hiring managers, recruiters, and peers. </p>
-
+		<p><strong>Purpose:</strong>
+			": This project will be a professional portfolio website with a focus on front-end web development concepts such as UX/UI."
+		</p>
+		<p>
+			<strong>Audience:</strong>
+			": This site is designed with a professional audience in mind.  Targeting individuals in the web development and I.T. community nationwide.
+			Target audience to include: startup founders, hiring managers, recruiters, and peers."
+		</p>
+		<p>
+			<strong>Goal:</strong>
+			": The primary goal of this site is to provide users with a direct way to contact and communicate with me in regards to employment and collaboration opportunities
+			through the contact form, and/or connected social media profiles.  The secondary goal of this site is to showcase my work, and to contribute to a polished professional online presence."
+		</p>
 	</body>
 </html>
