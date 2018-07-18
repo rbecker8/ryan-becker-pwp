20fcf7d58a439f0f3b5cad386d70254b548433d2
rbecker8 <rbecker8@cnm.edu>
2018-07-18 14:37:48 -0600

    Added name and age to persona.  Also added profession section.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -9,28 +9,33 @@
 	</header>
 	<body>
 		<h2>Purpose, Audience & Goal</h2>
-		<p><strong>Purpose:</strong>
+		<p><strong>Purpose</strong>
 			: This project will be a professional portfolio website with a focus on front-end web development concepts such as UX/UI.
 		</p>
 		<p>
-			<strong>Audience:</strong>
+			<strong>Audience</strong>
 			: This site is designed with a professional audience in mind.  Targeting individuals in the web development and I.T. community nationwide.
 			Target audience to include: startup founders, hiring managers, recruiters, and peers.
 		</p>
 		<p>
-			<strong>Goal:</strong>
+			<strong>Goal</strong>
 			: The primary goal of this site is to provide users with a direct way to contact and communicate with me in regards to employment and collaboration opportunities
 			through the contact form, and/or connected social media profiles.  The secondary goal of this site is to showcase my work, and to contribute to a polished professional online presence.
 		</p>
 		<h2>Persona</h2>
 		<p>
 			<strong>Name</strong>
-			:
+			: Bill Davis
 		</p>
 		<p>
 			<strong>Age</strong>
-			: et
-
+			:38
+		</p>
+		<p>
+			<strong>Profession</strong>
+			: Hiring manager at SolarCell, a startup that is looking to hire a full-time, in house Front-End Web Developer.  They have received funding from a few major corporations and are ready to
+			expand their website offerings to be better penetrate the tech and satellite market.  Their product line consists of deep space solar cells that power spacecraft and satellites.  They also offer wing
+			mounted solar cell systems for use in the ever expanding drone sector.
 		</p>
 	</body>
 </html>
