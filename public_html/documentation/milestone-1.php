66f1677892c0ae7f56b93ac49c46df6c9b3e3a3e
Ryan Becker <rbecker8@cnm.edugit config --global user.name Ryan>
2018-07-18 10:35:28 -0600

    Moved h1 into header and added purpose paragraph.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -4,9 +4,11 @@
 		<meta charset="UTF-8">
 		<title>PWP Milestone 1</title>
 	</head>
-	<h1 class="main-title">PWP Milestone 1</h1>
+	<header>
+		<h1>PWP Milestone 1</h1>
+	</header>
 	<body>
-
+		<p>Purpose: This project will be a professional portfolio website with a focus on front-end web development concepts such as UX/UI</p>
 
 
 	</body>
