cc3c3828cd2e4211f38feb868ff3cabd0e87d4bb
Ryan Becker <rbecker8@cnm.edugit config --global user.name Ryan>
2018-07-18 10:42:27 -0600

    Added audience and added emphasis to purpose and audience.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -8,8 +8,9 @@
 		<h1>PWP Milestone 1</h1>
 	</header>
 	<body>
-		<p>Purpose: This project will be a professional portfolio website with a focus on front-end web development concepts such as UX/UI</p>
-
+		<p><em>Purpose:</em> This project will be a professional portfolio website with a focus on front-end web development concepts such as UX/UI</p>
+		<p><em>Audience:</em> This site is designed with a professional audience in mind.  Targeting individuals in the web development and I.T. community nationwide.<br>
+		Target audience to include: startup founders, hiring managers, recruiters, and peers.</p>
 
 	</body>
 </html>
